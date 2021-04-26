3cca5442116a75f469746a32b96ab3f9ec253f80
Can Yumusak <can.yumusak@quartett-mobile.de>
2021-04-26 22:25:42 +0200

    fixed router

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ function index() {
     <div>
       <Appbar />
       <Router>
-        <InfoRoute />
+        <InfoRoute path="/user" />
       </Router>
       <Helmet>
         <meta charSet="utf-8" />
