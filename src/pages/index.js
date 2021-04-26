693a0b4d85f59bf2e76e81723538284af922a604
Can Yumusak <can.yumusak@quartett-mobile.de>
2021-04-26 20:51:21 +0200

    added router

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,17 @@ import "../style.css"
 import Appbar from "../components/appbar"
 import Info from "../components/info"
 import { Helmet } from "react-helmet"
+import { Router } from "@reach/router"
 
 function index() {
   return (
     <div>
       <Appbar />
-      <Info
-        path={"/user/:queriedUser"}
-      />
+      <Router>
+        <Info
+          path={"/user/:queriedUser"}
+        />
+      </Router>
       <Helmet>
         <meta charSet="utf-8" />
         <title>Home | Can Yumusak</title>
