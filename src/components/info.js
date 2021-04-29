3c901562a3cd35b96ee067a5348408c5f183c5f2
Can Yumusak <can.yumusak@quartett-mobile.de>
2021-04-29 20:04:45 +0200

    added bug chart

--- src/components/info.js
+++ src/components/info.js
@@ -1,14 +1,22 @@
-import React from 'react'
-import StoryPointChart from './storypointchart'
+import React from "react"
+import PerformanceLineChart from "./performanceLineChart"
 
 const info = (props) => {
   return (
-    <div className='container'>
-      <div className='display-3' style={{ color: '#FF8A00' }}>
+    <div className="container">
+      <div className="display-3" style={{ color: "#FF8A00" }}>
       </div>
-      <div className='h1 code mt-4 mb-3'>{'Story Point'}</div>
 
-      <StoryPointChart
+      <div className="h1 code mt-4 mb-3">{"Story Point"}</div>
+      <PerformanceLineChart
+        type="performance"
+        queriedUser={props.queriedUser} />
+
+      <div className="col-12 mb-sm-3" style={{ height: "40px" }} />
+
+      <div className="h1 code mt-20 mb-3">{"Bugs"}</div>
+      <PerformanceLineChart
+        type="bugs"
         queriedUser={props.queriedUser} />
     </div>
   )
